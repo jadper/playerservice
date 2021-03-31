a110418087cfb885a85785b5b4818695a05c57dc
jadper <per.carlsson@stud.sti.se>
2021-03-31 21:53:15 +0200

    mini

--- src/main/java/com/jadper/player/PlayerApplication.java
+++ src/main/java/com/jadper/player/PlayerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlayerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PlayerApplication.class, args);
-		
+		SpringApplication.run(PlayerApplication.class, args);	
 	}
 }
