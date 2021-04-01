061f633e7fba1dd416eb602317ea202f39c7262a
jadper <per.carlsson@stud.sti.se>
2021-04-01 22:14:28 +0200

    addet unittest

--- src/test/java/com/jadper/player/PlayerApplicationTests.java
+++ src/test/java/com/jadper/player/PlayerApplicationTests.java
@@ -1,5 +1,7 @@
 package com.jadper.player;
 
+import com.jadper.player.model.Player;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,36 @@ class PlayerApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void createPlayerAndSetName() {
+		String playerName = "Anna Alm";
+		Player p = new Player();
+		p.setName(playerName);
+		assert(p.getName()==playerName);
+	}
+
+	@Test
+	void createPlayerAndSetJersey() {
+		int jersey = 11;
+		Player p = new Player();
+		p.setJersey(jersey);
+		assert(p.getJersey()==jersey);
+	}
+	@Test
+	void createPlayerAndSetTeamId() {
+		int teamId = 1;
+		Player p = new Player();
+		p.setJersey(teamId);
+		assert(p.getJersey()==teamId);
+	}
+
+
+
+
+
+
+
 }
+
+
+
